546670dafb8b8ed0e700816d8659af56e21f85fe
A1093376liangliang <125643622+A1093376liangliang@users.noreply.github.com>
2024-02-22 21:37:10 +0800

    Week1 homework
    Week1 homework

---
+++ Week1_20240222/week1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "I love PHP!";
+
+        $a = 5;
+        $b = 3;
+
+        echo "<h1>I love HTML</h1>"
+    ?>
+    <h3>Hello PHP/HTML</h3>
+
+</body>
+</html>
